aed5b1e0df1b161b776392c76111dcc49f130b9e
Kristone Brown <Keyystone4@gmail.com>
2023-02-04 18:17:04 -0600

    Javascript outline

--- app.js
+++ app.js
@@ -1,14 +1,99 @@
-// 1) Define required constants
+// 1) Define required constants//
+const IMAGES = [
+    "https://i.imgur.com/AP5BYMO.png", //steelers
+    "https://i.imgur.com/m9pEfn3.png", //eagles
+    "https://i.imgur.com/TqDcQki.png", //chiefs
+    "https://i.imgur.com/ryP7Cst.png", //bengals
+    "https://i.imgur.com/QGCQxgJ.png", //packers
+    "https://i.imgur.com/wkXTDjI.png", //saints
 
-// 2) Define required variables used to track the state of the game
+    "https://i.imgur.com/AP5BYMO.png", //steelers
+    "https://i.imgur.com/m9pEfn3.png", //eagles
+    "https://i.imgur.com/TqDcQki.png", //chiefs
+    "https://i.imgur.com/ryP7Cst.png", //bengals
+    "https://i.imgur.com/QGCQxgJ.png", //packers
+    "https://i.imgur.com/wkXTDjI.png" //saints
+];
 
-// 3) Store elements on the page that will be accessed in code more than once in variables to make code more concise, readable and performant.
+// 2) ---- State variables ----
+let board; // array of 4 column arrays
+let winner; // player chooses all matching images under 1min 
 
-// 4) Upon loading the app should:
-//   4.1) Initialize the state variables
-//   4.2) Render those values to the page
-//   4.3) Wait for the user to click a square
 
-// 5) Handle a player clicking a square
+// 3) ---- Cached elements ---
+const messageEl = document.querySelector('h1');
+const restartBtn = document.querySelector('button');
+const timerEl = document.querySelector('timer');
+const squareEl = document.querySelectorAll('div');
 
-// 6) Handle a player clicking the replay button
+// 4) ---- Event listeners ---
+document.getElementById('board').addEventListener('click', handleMove);
+document.getElementById('div').addEventListener('click', handleToggle);
+timerEl.document.addEventListener('click', handleCountdown);
+restartBtn = document.addEventListener('click', initialize, handleShuffle);
+
+// 5) ---- functions ----
+initialize();
+    
+function initialize() {  
+    board = [
+        null, null, null, null,
+        null, null, null, null,
+        null, null, null, null
+    ];
+    winner = null;
+    render();
+};
+
+function handleMove() {
+
+};
+
+function handleToggle() {
+
+};
+
+function handleShuffle() {
+
+};
+
+function handleCountdown() {
+
+};
+
+// 6) ----- Visualize all state in the DOM ----
+function render() {
+    renderTimer();
+    renderBoard();
+    renderMessage();
+    renderResults();
+    //hide/show restart button
+    renderControls();
+
+function renderTimer() {
+    
+    timerEl.style.visibility = 'visible';
+    
+};
+
+function renderBoard() {
+
+};
+
+function renderMessage() {
+if(winner !==  ) {
+    messageEl.innerText = 'Wrong, Try Again !';
+} else if(winner) {
+    messageEl.innerText = 'Congratulations, You Win!!!';
+}
+};
+
+function renderResults() {
+
+};
+
+function renderControls() {
+    restartBtn.style.visibility = winner ? 'visible' : 'hidden';
+};
+
+};
